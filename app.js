805cb91d4b64c9dfd8a7ed49d0864897bcffe95c
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-12-14 10:20:41 -0800

    code for promting intern question added

--- app.js
+++ app.js
@@ -104,21 +104,12 @@ const promptManager = () => {
     })
 }
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const promptEngineer = () => {
+    inquirer.prompt(engineerQuestion).then(answer => {
+        employeeList.push(new Engineer(answer.name, answer.id, answer.email, answer.github));
+        promptSelection();
+    })
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
 
